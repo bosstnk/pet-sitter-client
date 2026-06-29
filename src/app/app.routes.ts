1c9c9145e7b98c8ec57eadd9790044e9fbb275d2
thanakorn <thanakorn.stsb@gmail.com>
2026-06-29 15:31:19 +0700

    refactor: remove style guide route from app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { StyleGuide } from './pages/style-guide/style-guide';
 
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'style', component: StyleGuide },
 ];
